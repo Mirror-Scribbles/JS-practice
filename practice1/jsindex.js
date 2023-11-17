415d634c2043b16d79aefdad37f35153362455d4
J andres <133991551+Mirror-Scribbles@users.noreply.github.com>
2023-11-17 12:18:57 -0500

    new

---
+++ practice1/jsindex.js
@@ -0,0 +1,20 @@
+const $button1 = document.querySelector('.button1');
+const $button2 = document.querySelector('.button2');
+
+const $manin = document.getElementById('mani_body');
+
+// $button1.addEventListener('click', () =>{
+//     console.log('this is working now')
+//     document.body.style.backgroundColor = 'red'
+// }) normal way to do the code, 
+
+$button1.addEventListener('click', () =>{
+if($manin.classList.contains('blue__color'))
+{$manin.classList.remove('blue__color')
+}else{$manin.classList.add('blue__color')}
+});
+
+// $button2.addEventListener('click', ()=>{
+// if($manin.classList.contains())
+// })
+
