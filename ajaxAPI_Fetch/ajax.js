c91dde0544c6e7484ce9a7cc8abd3d3282abd54c
J andres <133991551+Mirror-Scribbles@users.noreply.github.com>
2023-11-21 11:10:04 -0500

    update

--- ajaxAPI_Fetch/ajax.js
+++ ajaxAPI_Fetch/ajax.js
@@ -3,32 +3,59 @@
 
 // var request = $.ajax({
 //     method: "GET",
-//     URL: 'https://jsonplaceholder.typicode.com/posts'
+//     URL: 'https://jsonplaceholder.typicode.com/users'
 // }).done(console.log(request));
 
+// secound try : status pending. peticion ajax
 
-// secound try : status pending.
+// (() => {
+//     const $fetch = document.getElementById('fetch'), $fragment = document.createDocumentFragment();
 
-(() => {
-    const $fetch = document.getElementById('fetch'), $fragment = document.createDocumentFragment();
+//     fetch('https://jsonplaceholder.typicode.com/users')
+//     // .then((res) =>{
+//     //     console.log(res);
+//     //     return res.okay ? res.json(): Promise.reject(res);
+//     // })
+//     .then((res) => res.okay ? res.json(): Promise.reject(res))
+//     .then((json) => {
+//         console.log(json);
+//         // $fetch.innerHTML = json;
+//     })
+//     .catch((err) => {
+//         console.log(err)
+//         let message = err.statusText || 'there is an error'; 
+//         fetch.innerHTML = `Error ${err.status}: ${message}`;
+//     })
+//     .finally(() =>{
+//         console.log('This is the end of the code')
+//     })
+// })();
 
-    fetch('https://jsonplaceholder.typicode.com/users')
-    // .then((res) =>{
-    //     console.log(res);
-    //     return res.okay ? res.json(): Promise.reject(res);
-    // })
-    .then((res) => { return res.okay ? res.json() : Promise.reject(res);
-    })
-    .then((json) => {
-        console.log(json);
-        // $fetch.innerHTML = json;
-    })
-    .catch((err) => {
-        console.log(err)
-        let message = err.statusText || 'there is an error'; 
-        fetch.innerHTML = `Error ${err.status}: ${message}`;
-    })
-    .finally(() =>{
-        console.log('This is the end of the code')
-    })
-})();
+
+
+
+
+
+// third try pending, peticion : XMLHttpRequest. 
+
+
+const xhr = new XMLHttpRequest();
+xhr.open("GET", "https://jsonplaceholder.typicode.com/users");
+xhr.send();
+xhr.responseType = "json";
+xhr.onload = () => {
+  if (xhr.readyState == 4 && xhr.status == 200) {
+    const data = xhr.response;
+    console.log(data);
+  } else {
+    console.log(`Error: ${xhr.status}`);
+  }
+};
+
+
+
+
+
+
+
+
