0b3a3bc1d3fa4f88f69da99bfddff32c8bf47cf7
J andres <133991551+Mirror-Scribbles@users.noreply.github.com>
2024-01-04 13:07:55 -0500

    update

--- APIrest/API-REST.CRUD/CRUD_xmlhttprequest_ajax/index.js
+++ APIrest/API-REST.CRUD/CRUD_xmlhttprequest_ajax/index.js
@@ -4,3 +4,12 @@ $form = d.querySelector(".crud-form"),
 $title = d.querySelector(".crud-table"),
 $template = d.getElementById(".crud-template").content,
 $fragment = d.createDocumentFragment();
+
+const ajax = (Options) =>{
+    let {URL, method, success, data, error} = Options
+const xhr = new XMLHttpRequest();
+
+xhr.addEventListener('readystatechange', (e)=>{
+});
+xhr.open(method || 'GET', URL)
+}
