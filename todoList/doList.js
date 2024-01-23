91a7b3cca2e4159145800c1a3413fe0753b2990e
J andres <133991551+Mirror-Scribbles@users.noreply.github.com>
2024-01-23 13:10:12 -0500

    update

--- todoList/doList.js
+++ todoList/doList.js
@@ -1,3 +1,43 @@
-const $container = document.querySelector('#TaskContainer'),
-$btnAddTask = document.querySelector('btnAddTask'),
-$inputNewTask = document.querySelector('inputNewTask')
+const $input = document.querySelector('.form-control');
+const $buttonAddNewTask = document.querySelector('.btn-addTask');
+const $ul = document.querySelector('.TaskList');
+const $EmptyDiv = document.querySelector('.empty');
+
+$buttonAddNewTask.addEventListener('click', (e)=>{
+e.preventDefault();
+    console.log(e)
+
+    const text = $input.value;
+
+    if(text !==''){
+        const $li = document.createElement('li');
+        const $p = document.createElement('p');
+        $p.textContent = text;
+    
+        $li.appendChild($p);
+        $li.appendChild(AddDeleteBtn());
+        $ul.appendChild($li);
+    
+        $input.value ='';
+        $EmptyDiv.style.display = 'none';
+    }
+});
+
+const AddDeleteBtn = () =>{
+    const $deleteBtn = document.createElement('button');
+    $deleteBtn.textContent = 'X';
+    $deleteBtn.className = 'btn-delete';
+
+    $deleteBtn.addEventListener('click', (e)=>{
+        const item = e.target.parentElement;
+        $ul.removeChild(item);
+
+        const items = document.querySelectorAll('li');
+
+        if (items.length === 0){
+            $EmptyDiv.style.display = 'block'
+        }
+    });
+
+    return $deleteBtn;
+};
