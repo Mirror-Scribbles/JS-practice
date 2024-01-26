ca4be978305a2c34b1a330eeffc939ef76007a58
J andres <133991551+Mirror-Scribbles@users.noreply.github.com>
2024-01-26 11:50:24 -0500

    update

--- todoList/toDoList2/todo2.js
+++ todoList/toDoList2/todo2.js
@@ -5,48 +5,63 @@ $EmptyDivList = document.querySelector('.empty');
 
 const $title = document.querySelector('.title');
 
-$BtnAddNewTaskInput.addEventListener('click', (e) =>{
-    e.preventDefault();
-    // console.log(e);
+const $newLi = document.querySelector('lik');
 
-const content = $input.value;
+$BtnAddNewTaskInput.addEventListener('click', (event) =>{
+    event.preventDefault();
+    // console.log(event)
 
-if(!content.trim()){
-    console.log('form no valid')
-    $title.textContent = 'Empty form!';
-    
-    setTimeout( () =>{
-$title.textContent = 'form';
-    },2000)
-    return
-}if(content !== ''){
+    const value = $input.value;
+    if(!value.trim()){
+        $title.textContent = 'Empty form content!';
 
-    const $liContent = (task) => {
-        const liElement = `   
-        <li class="" id="">
-            <p class="" id="">
-                ${task}
-            </p>
-        </li>`
-
-    
-        $ulTaskList.insertAdjacentHTML('beforebegin', liElement);  
+        setTimeout(()=>{
+            $title.textContent = 'Form';
+        },2000)
     };
-    $BtnAddNewTaskInput.addEventListener('click', () => {
-        const task = $input.value;
-        if(task) {
-                addTask(task)
-        }
+    if(value){
+        const $liContent = document.createElement('li'),
+        $pContent = document.createElement('p');
+        $EmptyDivList.style.display = 'none'
+
+
+
+        $pContent.innerHTML = value;
+        $liContent.appendChild($pContent);
+        $liContent.appendChild(deleteButton());
+        $ulTaskList.appendChild($liContent);
+
+        $input.value = '';
+
     }
-    )
+})
 
-    // const $li = document.createElement('li')
-    // $li.classList.add('crossed-out');
-    // $li.textContent = content;
-    // console.log(content)
+const deleteButton = () =>{
+    const $deleteBtn = document.createElement('button');
+    $deleteBtn.textContent = 'X';
 
-// $ulTaskList.appendChild($li);
-$input.value = '';
-}
-}); 
-$liContent();
+    $deleteBtn.addEventListener('click', (e)=>{
+        const item = e.target.parentElement;
+        $ulTaskList.removeChild(item);
+
+        const items = document.querySelectorAll('li');
+
+        if (items.length === 0){
+            $EmptyDivList.style.display = 'block'
+        }
+    });
+
+    return $deleteBtn;
+};
+
+
+
+// if(!content.trim()){
+//     console.log('form no valid')
+//     $title.textContent = 'Empty form!';
+    
+//     setTimeout( () =>{
+// $title.textContent = 'form';
+//     },2000)
+//     return
+// }if(content !== ''){};
