4da52400b1e4dea527452b8e64f6cea687d10606
J andres <133991551+Mirror-Scribbles@users.noreply.github.com>
2024-02-07 11:55:49 -0500

    update

--- Dark-Light-Theme/ChangingThem1/dark_theme.js
+++
@@ -1,3 +0,0 @@
-export default function darkTheme(btn, classDark){
-
-}
--- Dark-Light-Theme/ChangingThem1/theme.js
+++
@@ -1,6 +0,0 @@
-const d = document;
-import darkTheme  from "./dark_theme";
-
-d.addEventListener('DOMContentLoaded', (e) =>{
-darkTheme('')
-})
