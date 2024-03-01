ba3b4265b0dadbae56c73f6d7f9b3837e8461175
J andres <133991551+Mirror-Scribbles@users.noreply.github.com>
2024-03-01 00:46:43 -0500

    update

---
+++ localStorage/localStorage.js
@@ -0,0 +1,11 @@
+// saving items onto the localStorage
+
+let mirror =  localStorage.setItem('mirror','andres');
+let mirror2 =  localStorage.setItem('mirror2','julian');
+
+// deleting one item from the localStorage
+// localStorage.removeItem('mirror');
+
+// deleting all elements from the localStorage
+
+// localStorage.clear();
