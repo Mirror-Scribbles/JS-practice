0df3851d813d5a39864984e18296031159c38e60
J andres <133991551+Mirror-Scribbles@users.noreply.github.com>
2024-03-14 13:08:30 -0500

    update

--- localStorage/localStorage.js
+++ localStorage/localStorage.js
@@ -1,11 +1,48 @@
 // saving items onto the localStorage
 
-let mirror =  localStorage.setItem('mirror','andres');
-let mirror2 =  localStorage.setItem('mirror2','julian');
+// let mirror =  localStorage.setItem('mirror','andres');
+// localStorage.setItem('mirror2','julian');
 
-// deleting one item from the localStorage
+// getting the item from local storage!
+
+// console.log(localStorage.getItem('mirror2'))
+
+// deleting one item from the localStorage.
 // localStorage.removeItem('mirror');
 
 // deleting all elements from the localStorage
 
 // localStorage.clear();
+
+
+// Creating a list and saving it onto the localStorage!
+
+// console.log(localStorage.getItem('mirror2'))
+
+// Lst = [{
+//     name: 'ector',
+//     age: 35
+// },
+// {
+//     name: 'carlos',
+//     age: 49
+// }
+// ];
+
+// localStorage.setItem('List', JSON.stringify(Lst))
+
+const List1 = JSON.parse(localStorage.getItem('List'));
+
+console.log(List1);
+
+List1.forEach(element => {
+let $documents = document.querySelector('.Dive');
+let $newDiv = document.createElement('div');
+
+$newDiv.innerHTML = `${element.name}, ${element.age}`
+
+$documents.appendChild($newDiv);
+});
+
+
+
