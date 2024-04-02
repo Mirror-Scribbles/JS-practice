4bfffc233797689fec7592e85b543b60f777ca76
J andres <133991551+Mirror-Scribbles@users.noreply.github.com>
2024-04-02 10:31:40 -0500

    update

--- Dark-Light-Theme/ChangingTheme3/them3.js
+++ Dark-Light-Theme/ChangingTheme3/them3.js
@@ -6,7 +6,17 @@ $selectors = d.querySelectorAll('[data-dark]');
 
 console.log($selectors)
 
-let moon = '☀️';
+let moon = '🌚';
 
 let sun = '☀️';
 
+d.addEventListener('click', (e)=>{
+if($buttonTheme.textContent === moon){
+$selectors.forEach(el => el.classList.add('dark-mode'))
+$buttonTheme.textContent = sun;
+}else{
+    $selectors.forEach(el => el.classList.remove('dark-mode'))
+    $buttonTheme.textContent = moon; 
+}
+})
+console.log($buttonTheme.textContent)
