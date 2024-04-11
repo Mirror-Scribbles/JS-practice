722dc136fdd038d3176c5d285f0d286304c22b3f
J andres <133991551+Mirror-Scribbles@users.noreply.github.com>
2024-04-11 13:55:52 -0500

    update

--- infinity-scroll/skroll1/infinity.js
+++ infinity-scroll/skroll1/infinity.js
@@ -1,2 +1,15 @@
 const $container = document.querySelector('.container');
 
+window.addEventListener('scroll', ()=>{
+
+    const {scrollHeight, clientHeight, scrollTop} = document.documentElement
+    
+    scrollTop + clientHeight > scrollHeight - 4 && setTimeout(newContainer, 1000)
+
+})
+
+const newContainer = ()=> {
+    const $NowBox = document.createElement('div')
+    $NowBox.className = 'love-death-robot g'
+    $container.appendChild($NowBox);
+}
