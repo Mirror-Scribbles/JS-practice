3c7dc8b21a24094e8cd724aab3ea119ca574f7fd
J andres <133991551+Mirror-Scribbles@users.noreply.github.com>
2024-04-17 06:50:10 -0500

    update

--- infinity-scroll/infinite-skroll-horizontal/skroller1/skroller3.js
+++ infinity-scroll/infinite-skroll-horizontal/skroller1/skroller3.js
@@ -6,6 +6,15 @@ addAnimation();
 
 function addAnimation(){
 $scroller.forEach((scroller) =>{
-    scroller.setAttribute("data-animated", true)
+    scroller.setAttribute("data-animated", true);
+
+const scrollerInner = scroller.querySelector('.scroller__inner');
+const scrollerContent = Array.from(scrollerInner.children);
+
+scrollerContent.forEach(item =>{ 
+    const duplicateItem = item.cloneNode(true);
+    duplicateItem.setAttribute("area-hidden", true);
+    scrollerInner.appendChild(duplicateItem)
+})
 })
 }
