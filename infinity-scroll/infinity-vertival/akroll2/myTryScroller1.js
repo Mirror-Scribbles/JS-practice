e73c5a3fa9331e63de9cd702c2d7cdd6eb5dbb54
J andres <133991551+Mirror-Scribbles@users.noreply.github.com>
2024-04-25 22:48:46 -0500

    update

--- infinity-scroll/infinity-vertival/akroll2/myTryScroller1.js
+++ infinity-scroll/infinity-vertival/akroll2/myTryScroller1.js
@@ -5,23 +5,27 @@ const $divSpinner = document.querySelector('.div-spinner')
 window.addEventListener('scroll', ()=>{
     const {scrollHeight, clientHeight, scrollTop} = document.documentElement;
 
-    scrollTop + clientHeight >= scrollHeight - 4 && setTimeout(loader, 1000);
+    if(scrollTop + clientHeight >= scrollHeight - 4){
+        
+        setTimeout(()=>{
+            const loader = () =>{
 
-    $spinner.classList.add('.spinner-hidden');
-    $spinner.addEventListener('transitioned', (e)=>{
-        document.body.removeChild($divSpinner)
+                const $KillerMorpheusUl = document.querySelector('.Killer-morpheus');
+                const KillerMorpheusContent = Array.from($KillerMorpheusUl.children)
+                KillerMorpheusContent.forEach(item =>{
+                
+                    const duplicateNode = item.cloneNode(true)
+                    $KillerMorpheusUl.appendChild(duplicateNode)
+                })
+                }
+                loader();
+        }, 2000)
 
-        console.log(e)
-    })
+    }if(scrollTop + clientHeight >= scrollHeight - 4){
+        $spinner.classList.add('spinner-hidden')
+    }else{
+        $spinner.classList.remove('spinner-hidden')  
+    }
+    
 })
 
-const loader = () =>{
-
-const $KillerMorpheusUl = document.querySelector('.Killer-morpheus');
-const KillerMorpheusContent = Array.from($KillerMorpheusUl.children)
-KillerMorpheusContent.forEach(item =>{
-
-    const duplicateNode = item.cloneNode(true)
-    $KillerMorpheusUl.appendChild(duplicateNode)
-})
-}
