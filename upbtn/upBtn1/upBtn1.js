d3aed1de3100c5a99bc8ccd5f4090c0bc3eb2851
J andres <133991551+Mirror-Scribbles@users.noreply.github.com>
2024-05-10 12:46:58 -0500

    update

--- upbtn/upBtn1/upBtn1.js
+++ upbtn/upBtn1/upBtn1.js
@@ -2,4 +2,19 @@ const $btnUpButton = document.querySelector('.bnt-up');
 
 const $IconBtn = document.querySelector('.Icon-bnt-up')
 
-const YIp = document.d
+
+document.addEventListener('scroll', ()=>{
+    const YIp = document.documentElement.scrollTop;
+    if(YIp === 0){
+        $btnUpButton.classList.remove('hide')
+        $btnUpButton.classList.add('show')
+        console.log('try one')
+        
+    } else if(YIp >= 100)
+    {
+        console.log('try two')
+        $btnUpButton.classList.remove('show')
+        $btnUpButton.classList.add('hide')
+    }
+
+})
