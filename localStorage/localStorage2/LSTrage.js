503cadcd5736165a4bd7dfb87baaeba20c429c36
J andres <133991551+Mirror-Scribbles@users.noreply.github.com>
2024-05-23 10:28:48 -0500

    update

--- localStorage/localStorage2/LSTrage.js
+++ localStorage/localStorage2/LSTrage.js
@@ -2,15 +2,31 @@ const $backgroundBtn  = document.querySelector('.btn-BackGround');
 const $buttonCleaner = document.querySelector('.clean-btn');
 
 
-
-
 const setBg = () => {
     const randomColor = Math.floor(Math.random()*16777215).toString(16);
     document.body.style.backgroundColor = "#" + randomColor;
-    color.innerHTML = "#" + randomColor;
+    document.innerHTML = "#" + randomColor;
     }
+    $backgroundBtn.addEventListener('click', setBg)
 
-    $backgroundBtn.addEventListener("click", (e) =>{
-        const backColor = localStorage.setItem(setBg, 'My')
-        const GetBackColor = localStorage.getItem(setBg())
-    });
+
+    // const $button1 = document.querySelector('.button1');
+    // const $button2 = document.querySelector('.button2');
+    // const $button3 = document.querySelector('.button3');
+    // const $button4 = document.querySelector('.button4');
+
+    // $button1.addEventListener('click', () => {
+    //     console.log("you did click in the first button")
+    // });
+
+    // $button2.addEventListener('click', () => {
+    //     console.log("you did click in the second button")
+    // });
+
+    // $button3.addEventListener('click', () => {
+    //     console.log("you did click in the third button")
+    // });
+
+    // $button4.addEventListener('click', () => {
+    //     console.log("you did click in the fourth button")
+    // });
